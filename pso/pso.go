ffe8fe3b823c676c3523043389e2d89d694cb1f4
aagoldingay <aarongoldingay@hotmail.com>
2018-11-11 23:05:45 +0000

    added velocity methods

--- pso/pso.go
+++ pso/pso.go
@@ -3,6 +3,7 @@ package pso
 import (
 	"log"
 	"math/rand"
+	"time"
 
 	pp "github.com/aagoldingay/ci-cw-go/pricingproblem"
 )
@@ -23,18 +24,42 @@ type Particle struct {
 
 // NewParticle generates and returns a new instanc of a particle
 // With a randomly generated design
-func NewParticle(numberOfGoods int, pr *pp.PricingProblem) *Particle {
+func NewParticle(numGoods int, pr *pp.PricingProblem) *Particle {
 	p := new(Particle)
-	p.prices = randomPrices(numberOfGoods)
-	p.velocity = initialVelocity(p.prices, randomPrices(numberOfGoods))
+	p.prices = randomPrices(numGoods, pr)
+	p.velocity = initialVelocity(p.prices, randomPrices(numGoods, pr))
 	p.bestPrices = make([]float64, len(p.prices))
 	copy(p.bestPrices, p.prices) //important to copy due to pass by reference
-	p.currentRevenue = evaluatePrices(numberOfGoods, p.prices, pr)
+	p.currentRevenue = evaluatePrices(p.prices, pr)
 	p.bestRevenue = p.currentRevenue
 	return p
 }
 
-func evaluatePrices(numberOfGoods int, prices []float64, pr *pp.PricingProblem) float64 {
+// Update handles the repositioning and evaluation of a particle
+// param: gBestPrices passes information of the global best prices across a whole population of particles
+func (p *Particle) Update(numGoods int, gBestPrices []float64, pr *pp.PricingProblem) {
+	copy(p.velocity, calculateVelocity(p.velocity, p.prices, p.bestPrices, gBestPrices))
+	//copy(p.prices, updatePosition(p.prices, p.velocity, numGoods))
+	p.currentRevenue = evaluatePrices(p.prices, pr)
+	if p.currentRevenue < p.bestRevenue {
+		copy(p.bestPrices, p.prices)
+		p.bestRevenue = p.currentRevenue
+	}
+}
+
+// calculateVelocity calculates the movement properties ready for updating a Particle's position
+// uses inertia, cognitiveW, socialW constants
+func calculateVelocity(velocity, prices, pBestPrices, gBestPrices []float64) []float64 {
+	newVelocity := make([]float64, len(velocity))
+	for i := 0; i < len(velocity); i++ {
+		rand.Seed(time.Now().UnixNano())
+		r1, r2 := rand.Float64(), rand.Float64()
+		newVelocity[i] = (inertia * velocity[i]) + (cognitiveW * r1 * (pBestPrices[i] - prices[i])) + (socialW * r2 * (gBestPrices[i] - prices[i]))
+	}
+	return newVelocity
+}
+
+func evaluatePrices(prices []float64, pr *pp.PricingProblem) float64 {
 	revenue, err := pr.Evaluate(prices)
 	if err != nil {
 		log.Fatal(err)
@@ -51,10 +76,13 @@ func initialVelocity(firstPrices, secondPrices []float64) []float64 {
 	return velocity
 }
 
-func randomPrices(numberOfGoods int) []float64 {
-	prices := make([]float64, numberOfGoods)
-	for i := 0; i < numberOfGoods; i++ {
-		prices[i] = rand.Float64() * 10
+// randomPrices generates random prices that evaluated as valid by the PricingProblem
+func randomPrices(numGoods int, pr *pp.PricingProblem) []float64 {
+	prices := make([]float64, numGoods)
+	for !pr.IsValid(prices) {
+		for i := 0; i < numGoods; i++ {
+			prices[i] = rand.Float64() * 10
+		}
 	}
 	return prices
 }
