ae6f862eba5c3798b0601629d3f3cea499a9a11d
aagoldingay <aarongoldingay@hotmail.com>
2018-12-03 23:07:05 +0000

    changes to data

--- algorithms/algorithms.go
+++ algorithms/algorithms.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"time"
 
 	"github.com/aagoldingay/ci-cw-go/ais"
 	pp "github.com/aagoldingay/ci-cw-go/pricingproblem"
@@ -22,18 +23,32 @@ type Revenue struct {
 func AISSearch(numGoods, numPopulation, replacement, cloneSizeFactor int, trace bool, p *pp.PricingProblem) (float64, []float64) {
 	revenueTrack := []float64{}
 	population := ais.NewImmuneSystem(numGoods, numPopulation, replacement, cloneSizeFactor, p)
-	fmt.Printf("best cell: %v\n", population.BestCell)
+	fmt.Printf("Cells created...\n")
+	//fmt.Printf("best cell: %v\n", population.BestCell)
 
-	for i := 0; i < 100; i++ {
-		population.Update()
-		fmt.Printf("[%v] best cell : %v\n", i+1, population.BestCell)
+	timeout := time.After(3 * time.Second)
+	tick := time.Tick(5 * time.Millisecond)
 
-		// only track if required
-		if trace {
-			revenueTrack = append(revenueTrack, population.BestCell.Revenue)
+	for { //for i := 0; i < 100; i++ {
+		// if testing with time, uncomment switch statement, move inner case <-timeout out of for loop
+
+		select {
+		// timeout reached, stop running
+		case <-timeout:
+			fmt.Printf("Final best revenue : %v\n", population.BestCell)
+			revenueTrack = append(revenueTrack, population.BestCell.Revenue) // adds 30th result
+			return population.BestCell.Revenue, revenueTrack
+		// tick reached, record data
+		case <-tick:
+			if trace {
+				revenueTrack = append(revenueTrack, population.BestCell.Revenue)
+			}
+		// run procedure
+		default:
+			population.Update()
+			//fmt.Printf("[%v] best cell : %v\n", i+1, population.BestCell) //uncomment on iterations
 		}
 	}
-	return population.BestCell.Revenue, revenueTrack
 }
 
 // PSOSearch is a CI algorithm approach to finding the highest possible revenue
@@ -42,18 +57,31 @@ func PSOSearch(numGoods, numParticles int, trace bool, p *pp.PricingProblem) (fl
 	revenueTrack := []float64{}
 	swarm := pso.NewSwarm(numGoods, numParticles, p)
 	fmt.Printf("Particles created...\n")
-	fmt.Printf("Best : %v | %v\n", swarm.BestPrices, swarm.BestRevenue)
+	//fmt.Printf("Best : %v | %v\n", swarm.BestPrices, swarm.BestRevenue)
 
-	for i := 0; i < 100; i++ {
-		swarm.Update()
-		fmt.Printf("[%v] new best: prices : %v | revenue : %v\n", i+1, swarm.BestPrices, swarm.BestRevenue)
+	timeout := time.After(3 * time.Second)
+	tick := time.Tick(5 * time.Millisecond)
 
-		// only track if required
-		if trace {
-			revenueTrack = append(revenueTrack, swarm.BestRevenue)
+	for { //for i := 0; i < 100; i++ {
+		// if testing with time, uncomment switch statement, move inner case <-timeout out of for loop
+
+		select {
+		// timeout reached, stop running
+		case <-timeout:
+			fmt.Printf("Final best revenue : %v\n", swarm.BestRevenue)
+			revenueTrack = append(revenueTrack, swarm.BestRevenue) // adds 30th result
+			return swarm.BestRevenue, revenueTrack
+		// tick reached, record data
+		case <-tick:
+			if trace {
+				revenueTrack = append(revenueTrack, swarm.BestRevenue)
+			}
+		// run procedure
+		default:
+			swarm.Update()
+			//fmt.Printf("[%v] new best: prices : %v | revenue : %v\n", i+1, swarm.BestPrices, swarm.BestRevenue) // uncomment if steps		}
 		}
 	}
-	return swarm.BestRevenue, revenueTrack
 }
 
 // RandomSearch is a heuristic method of attempting to find the highest possible revenue
@@ -73,27 +101,39 @@ func RandomSearch(numGoods int, trace bool, p *pp.PricingProblem) (float64, []fl
 	if err != nil {
 		log.Fatal(err)
 	}
-	for i := 0; i < 100; i++ {
 
-		for j := 0; j < numGoods; j++ {
-			newPrices[j] = rand.Float64() * 10
-		}
+	timeout := time.After(3 * time.Second)
+	tick := time.Tick(5 * time.Millisecond)
 
-		newRevenue, err := p.Evaluate(newPrices)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if newRevenue > bestRevenue.revenue {
-			copy(bestRevenue.prices, newPrices)
-			bestRevenue.revenue = newRevenue
-			fmt.Printf("New best revenue : %v \n", bestRevenue)
-		}
+	for { //for i := 0; i < 100; i++ {
+		// if testing with time, uncomment switch statement, move inner case <-timeout out of for loop
 
-		// only track if required
-		if trace {
-			revenueTrack = append(revenueTrack, bestRevenue.revenue)
+		select {
+		// timeout reached, stop running
+		case <-timeout:
+			fmt.Printf("Final best revenue : %v\n", bestRevenue)
+			revenueTrack = append(revenueTrack, bestRevenue.revenue) // adds 30th result
+			return bestRevenue.revenue, revenueTrack
+		// tick reached, record data
+		case <-tick:
+			if trace {
+				revenueTrack = append(revenueTrack, bestRevenue.revenue)
+			}
+		// run procedure
+		default:
+			for j := 0; j < numGoods; j++ {
+				newPrices[j] = rand.Float64() * 10
+			}
+
+			newRevenue, err := p.Evaluate(newPrices)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if newRevenue > bestRevenue.revenue {
+				copy(bestRevenue.prices, newPrices)
+				bestRevenue.revenue = newRevenue
+				//fmt.Printf("New best revenue : %v \n", bestRevenue)
+			}
 		}
 	}
-	fmt.Printf("Final best revenue : %v\n", bestRevenue)
-	return bestRevenue.revenue, revenueTrack
 }
--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/aagoldingay/ci-cw-go/algorithms"
 	pp "github.com/aagoldingay/ci-cw-go/pricingproblem"
+	"github.com/aagoldingay/ci-cw-go/xlsxhandler"
 )
 
 func main() {
 	numGoods := 20
-	// seeds := []int64{0, 38, 113} // simple, for parameter configuration
+	//seeds := []int64{0, 38, 113} // simple, for parameter configuration
 	seeds := []int64{0, 38, 113, 100, 50, 25, 75, 13, 55, 98, 187, 4, 12, 42, 66, 72, 30, 32, 10, 24, 49, 35, 88, 61, 19, 23, 14, 91, 102, 147}
 
-	//runSingle(numGoods, seeds[0])
+	// runSingle(numGoods, seeds[0])
 	runAll(numGoods, seeds)
 }
 
@@ -21,7 +22,8 @@ func runSingle(numGoods int, seed int64) {
 	p = *p.MakeProblem(numGoods, seed, false) //courseworkInstance
 	// p = *p.MakeProblem(numGoods, seed, true) //randomInstance
 
-	algorithms.RandomSearch(numGoods, false, &p) //numGoods
+	rev, history := algorithms.RandomSearch(numGoods, true, &p) //numGoods
+	fmt.Printf("rev : %v\nall : %v\n", rev, history)
 	// algorithms.PSOSearch(numGoods, 25, false, &p) //numGoods, numParticles
 	// algorithms.AISSearch(numGoods, 30, 10, 5, false, &p) //numGoods, numPopulation, replacement, cloneSizeFactor
 }
@@ -35,9 +37,9 @@ func runAll(numGoods int, seeds []int64) {
 
 	// revenue trackers
 	finalRevenues := [][]float64{}
-	// randomRevenues := [][]float64{}
-	// psoRevenues := [][]float64{}
-	// aisRevenues := [][]float64{}
+	randomRevenues := [][]float64{}
+	psoRevenues := [][]float64{}
+	aisRevenues := [][]float64{}
 
 	for i := 0; i < 3; i++ { // 3 algorithms
 		finalRevenues = append(finalRevenues, make([]float64, len(seeds)))
@@ -49,23 +51,23 @@ func runAll(numGoods int, seeds []int64) {
 		// p = *p.MakeProblem(numGoods, true) //randomInstance
 
 		// data structures for returned list of revenues per step of each process (for xlsx printing)
-		//var ran, pso, ais []float64
+		var ran, pso, ais []float64
 
 		fmt.Printf("----------\nRandom Search\n----------\n")
-		finalRevenues[0][i], _ = algorithms.RandomSearch(numGoods, false, &p)
-		//randomRevenues = append(randomRevenues, ran)
+		finalRevenues[0][i], ran = algorithms.RandomSearch(numGoods, true, &p)
+		randomRevenues = append(randomRevenues, ran)
 
 		fmt.Printf("----------\nPSO\n----------\n")
-		finalRevenues[1][i], _ = algorithms.PSOSearch(numGoods, psoPopulation, false, &p)
-		//psoRevenues = append(psoRevenues, pso)
+		finalRevenues[1][i], pso = algorithms.PSOSearch(numGoods, psoPopulation, true, &p)
+		psoRevenues = append(psoRevenues, pso)
 
 		fmt.Printf("----------\nAIS\n----------\n")
-		finalRevenues[2][i], _ = algorithms.AISSearch(numGoods, aisPopulation, aisReplacement, aisClonesFactor, false, &p) //numGoods, numPopulation, replacement, cloneSizeFactor
-		//aisRevenues = append(aisRevenues, ais)
+		finalRevenues[2][i], ais = algorithms.AISSearch(numGoods, aisPopulation, aisReplacement, aisClonesFactor, true, &p) //numGoods, numPopulation, replacement, cloneSizeFactor
+		aisRevenues = append(aisRevenues, ais)
 	}
 	fmt.Printf("%v\n", finalRevenues)
 
 	// xlsx output
 	// xlsxhandler.WriteXLSXParams(finalRevenues, psoPopulation, aisPopulation, aisReplacement, aisClonesFactor)
-	// xlsxhandler.WriteXLSXRevenues(seeds, randomRevenues, psoRevenues, aisRevenues)
+	xlsxhandler.WriteXLSXRevenues(seeds, randomRevenues, psoRevenues, aisRevenues)
 }
